e779bb5961bea4afd70fe7a67640bf3faf1cfa4b
Alon Catz <alon.catz@gmail.com>
2015-01-17 22:09:10 +0200

    Add AssertEx class

---
+++ WcfClient.Test/TestFramework/AssertEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reeb.Wcf.Test.TestFramework
+{
+    public static class AssertEx
+    {
+        public static async Task Throws<TException>(Func<Task> func) where TException : Exception
+        {
+            try
+            {
+                await func();
+                Assert.Fail("There was no exception. Expected exception of type {0}", typeof (TException).Name);
+            }
+            catch (TException)
+            {
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Received exception of type {0}. Expected exception of type {1}", ex.GetType().Name,
+                    typeof (TException).Name);
+            }
+        }
+    }
+}
